93bc90ec524bffa7371182bb23af1a9ccc83942e
olanderb <billyolander@gmail.com>
2023-05-16 11:37:19 -0600

    add scripts for key indicators

---
+++ scripts/FCSN_Admin1.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(labelled)
+library(expss)
+library(haven)
+library(officer)
+
+#wfp themes package will be updated frequently - so its good to reinstall it everytime for now
+library(devtools)
+install_github("WFP-VAM/wfpthemes")
+library(wfpthemes)
+
+
+#load sample data set
+data <- haven::read_sav("data/sampledataenglish.sav")
+
+#calculate FCSN - script copied and pasted from https://github.com/WFP-VAM/RAMResourcesScripts/blob/main/Indicators/Food-consumption-score-nutrition/FCSN_indicator_tidyverse.R
+
+#recode "n/a" values to 0
+vars2recode <- c("FCSNPrMeatF","FCSNPrMeatO","FCSNPrFish","FCSNPrEggs","FCSNVegOrg","FCSNVegGre","FCSNFruiOrg")
+
+data <- data %>% mutate_at(vars2recode, ~replace(., . == "n/a", "0"))
+
+data <- data %>% mutate_at(vars2recode, as.numeric)
+
+
+#compute aggregates of key micronutrient consumption of vitamin, iron and protein 
+data <- data %>% mutate(FGVitA = FCSDairy +FCSNPrMeatO +FCSNPrEggs +FCSNVegOrg +FCSNVegGre +FCSNFruiOrg)
+var_label(data$FGVitA) <- "Consumption of vitamin A-rich foods"
+
+data <- data %>% mutate(FGProtein = FCSPulse +FCSDairy +FCSNPrMeatF +FCSNPrMeatO +FCSNPrFish +FCSNPrEggs)
+var_label(data$FGProtein) <- "Consumption of protein-rich foods"
+
+data <- data %>% mutate(FGHIron = FCSNPrMeatF +FCSNPrMeatO +FCSNPrFish)
+var_label(data$FGHIron) <- "Consumption of hem iron-rich foods"
+
+#recode into nutritious groups  
+data <- data %>% mutate(FGVitACat = case_when(FGVitA == 0 ~ 1, between(FGVitA,1,6) ~ 2, FGVitA >= 7 ~ 3),
+                        FGProteinCat = case_when(FGProtein == 0 ~ 1, between(FGProtein,1,6) ~ 2,  FGProtein >= 7 ~ 3),
+                        FGHIronCat = case_when(FGHIron == 0 ~ 1, between(FGHIron,1,6) ~ 2,  FGHIron >= 7 ~ 3)
+)
+
+
+# define variables labels and properties for FGVitACat FGProteinCat FGHIronCat
+data <- data %>%
+  mutate(across(c(FGVitACat, FGProteinCat, FGHIronCat), ~labelled(., labels = c(
+    "Never consumed" = 1,
+    "Consumed sometimes" = 2,
+    "Consumed at least 7 times" = 3
+  ))))
+
+
+#create table of FGVitACat, FGProteinCat, FGHIronCat by ADM1
+
+
+#make plot
+rcsi_barplot <- rcsi_admin1_table_long %>% 
+  ggplot() +geom_col(aes(x = ADMIN1Name, y = perc,fill = FCSCat21), width = 0.7) +geom_text(aes(x = ADMIN1Name,
+                                                                                                y = perc,
+                                                                                                color = FCSCat21,
+                                                                                                label = perc),
+                                                                                            position = position_stack(vjust = 0.5),
+                                                                                            show.legend = FALSE,
+                                                                                            size = 10/.pt,
+  )+ scale_color_manual(values = c(main_white, main_black, main_white)) +
+  labs(
+    title = "Household Food Consumption Score Classification by State | April 2023",
+    subtitle = "Relative Proportion of Households per FCS Classification by State in Fake Country",
+    caption = "Source: Emergency Food Security Assessment, data collected April 2023"
+  )  +  scale_fill_wfp_b(palette = "pal_stoplight_3pt") + theme_wfp(grid = "XY",
+                                                                    axis = FALSE,
+                                                                    axis_title = FALSE)
+
+
--- scripts/rCSI_Admin1.R
+++ scripts/rCSI_Admin1.R
@@ -13,32 +13,23 @@ library(wfpthemes)
 #load sample data set
 data <- haven::read_sav("data/sampledataenglish.sav")
 
-#calculate FCS & FCG - script copied and pasted from https://github.com/WFP-VAM/RAMResourcesScripts/blob/main/Indicators/Food-consumption-score/FCS_indicator_tidyverse.R
-#calculate FCS
-data <- data %>% mutate(FCS = (2 * FCSStap) +(3 * FCSPulse) +(4*FCSPr) +(4*FCSDairy) +FCSVeg  +FCSFruit +(0.5*FCSFat) +(0.5*FCSSugar))
-var_label(data$FCS) <- "Food Consumption Score"
-#create FCG groups based on 21/25 or 28/42 thresholds
-#Use this when analyzing a country with low consumption of sugar and oil - thresholds 21-35
-data <- data %>% mutate(FCSCat21 = case_when(
-  FCS <= 21 ~ 1, between(FCS, 21.5, 35) ~ 2, FCS > 35 ~ 3))
-val_lab(data$FCSCat21) = num_lab("
-             1 Poor
-             2 Borderline
-             3 Acceptable
-")
-var_label(data$FCSCat21) <- "FCS Categories"
+#calculate rCSI - script copied and pasted from https://github.com/WFP-VAM/RAMResourcesScripts/blob/main/Indicators/Reduced-coping-strategy-index/rCSI_tidyverse.R
+#calculate reduced Coping Strategy Index (rCSI)
+data <- data %>% mutate(rCSI = rCSILessQlty + (2 * rCSIBorrow) + rCSIMealNb + rCSIMealSize + (3 * rCSIMealAdult))
+var_label(data$rCSI) <- "Reduced coping strategies index (rCSI)"
 
 
-#make table of FCG by admin1 - where should this script go - in with indicator calculation or with the viz?
-fcscat21_admin1_table_long <- data %>% 
-  group_by(ADMIN1Name = to_factor(ADMIN1Name)) %>%
-  count(FCSCat21 = as.character(FCSCat21)) %>%
-  mutate(perc = 100 * n / sum(n)) %>%
-  ungroup() %>% select(-n) %>% mutate_if(is.numeric, round, 1) 
+#creates a table of the unweighted mean of rCSI
+
+
+#unweighted mean rCSI by ADM1
+rcsi_admin1_table_long <- data %>% group_by(ADMIN1Name) %>% 
+  drop_na(rCSI) %>% 
+  summarise(meanrCSI = mean(rCSI))
 
 
 #make plot
-fcscat21_barplot <- fcscat21_admin1_table_long %>% 
+rcsi_barplot <- rcsi_admin1_table_long %>% 
   ggplot() +geom_col(aes(x = ADMIN1Name, y = perc,fill = FCSCat21), width = 0.7) +geom_text(aes(x = ADMIN1Name,
                                                                                                 y = perc,
                                                                                                 color = FCSCat21,
