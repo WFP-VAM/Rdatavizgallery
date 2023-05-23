9ebf36b6de60b6453d9b15a34f0588b2df9ed81f
nicolefindstar <yue.wu@berkeley.edu>
2023-05-23 11:56:06 +0200

    Update FCSN_Admin1.R

--- scripts/FCSN_Admin1.R
+++ scripts/FCSN_Admin1.R
@@ -1,77 +1,138 @@
+#------------------------------------------------------------------------------#
 
+#	                        WFP RAM Standardized Scripts
+#                      Calculating and Visualizing FCSN
 
+#------------------------------------------------------------------------------#
+
+## Last updated:  May 23 2023
+
+## Load Packages --------------------------------------------------------------#
 
 library(tidyverse)
+library(dplyr)
 library(labelled)
 library(expss)
 library(haven)
 library(officer)
-
-#wfp themes package will be updated frequently - so its good to reinstall it everytime for now
+library(gtsummary)
+# WFP themes package will be updated frequently - reinstall it everytime for now
 library(devtools)
 install_github("WFP-VAM/wfpthemes")
 library(wfpthemes)
 
-#load sample data set
-data <- haven::read_sav("data/sampledataenglish.sav")
+# Load Sample Data ------------------------------------------------------------#
 
-#calculate FCSN - script copied and pasted from https://github.com/WFP-VAM/RAMResourcesScripts/blob/main/Indicators/Food-consumption-score-nutrition/FCSN_indicator_tidyverse.R
+data <- read_csv("data/FCSN_Sample_Survey.csv")
 
-#recode "n/a" values to 0
-vars2recode <- c("FCSNPrMeatF","FCSNPrMeatO","FCSNPrFish","FCSNPrEggs","FCSNVegOrg","FCSNVegGre","FCSNFruiOrg")
+# Calculate FCSN --------------------------------------------------------------# 
+# Script copied and pasted from 
+# https://github.com/WFP-VAM/RAMResourcesScripts/blob/main/Indicators/Food-consumption-score-nutrition/FCSN_indicator_tidyverse.R
+
+#recode "n/a" values to 0 and change to numeric
+vars2recode <- c("FCSNPrMeatF","FCSNPrMeatO","FCSNPrFish","FCSNPrEggs",
+                 "FCSNVegOrg","FCSNVegGre","FCSNFruiOrg")
 
 data <- data %>% mutate_at(vars2recode, ~replace(., . == "n/a", "0"))
-
 data <- data %>% mutate_at(vars2recode, as.numeric)
 
-#compute aggregates of key micronutrient consumption of vitamin, iron and protein 
-data <- data %>% mutate(FGVitA = FCSDairy +FCSNPrMeatO +FCSNPrEggs +FCSNVegOrg +FCSNVegGre +FCSNFruiOrg)
+# Compute aggregates of key micronutrient consumption -------------------------#
+
+## Vitamin A-Rich Foods -------------------------------------------------------#
+
+data <- data %>% mutate(FGVitA = FCSDairy +FCSNPrMeatO +FCSNPrEggs +
+                                 FCSNVegOrg +FCSNVegGre +FCSNFruiOrg)
 var_label(data$FGVitA) <- "Consumption of vitamin A-rich foods"
 
-data <- data %>% mutate(FGProtein = FCSPulse +FCSDairy +FCSNPrMeatF +FCSNPrMeatO +FCSNPrFish +FCSNPrEggs)
+## Protein-Rich Foods ---------------------------------------------------------#
+
+data <- data %>% mutate(FGProtein = FCSPulse +FCSDairy +FCSNPrMeatF +
+                                    FCSNPrMeatO +FCSNPrFish +FCSNPrEggs)
 var_label(data$FGProtein) <- "Consumption of protein-rich foods"
+
+## Iron-Rich Foods ------------------------------------------------------------#
 
 data <- data %>% mutate(FGHIron = FCSNPrMeatF +FCSNPrMeatO +FCSNPrFish)
 var_label(data$FGHIron) <- "Consumption of hem iron-rich foods"
 
-#recode into nutritious groups  
-data <- data %>% mutate(FGVitACat = case_when(FGVitA == 0 ~ 1, between(FGVitA,1,6) ~ 2, FGVitA >= 7 ~ 3),
-                        FGProteinCat = case_when(FGProtein == 0 ~ 1, between(FGProtein,1,6) ~ 2,  FGProtein >= 7 ~ 3),
-                        FGHIronCat = case_when(FGHIron == 0 ~ 1, between(FGHIron,1,6) ~ 2,  FGHIron >= 7 ~ 3)
-)
+## recode into nutritious groups  ---------------------------------------------#
+
+data <- data %>% mutate(FGVitACat = case_when(FGVitA == 0 ~ 1,
+                                    between(FGVitA,1,6)   ~ 2, 
+                                    FGVitA >= 7           ~ 3),
+                        FGProteinCat = case_when(FGProtein == 0 ~ 1, 
+                                       between(FGProtein,1,6)   ~ 2,
+                                       FGProtein >= 7           ~ 3),
+                        FGHIronCat = case_when(FGHIron == 0 ~ 1,
+                                     between(FGHIron,1,6)   ~ 2,
+                                     FGHIron >= 7           ~ 3))
 
 # define variables labels and properties for FGVitACat FGProteinCat FGHIronCat
+
 data <- data %>%
-  mutate(across(c(FGVitACat, FGProteinCat, FGHIronCat), ~labelled(., labels = c(
-    "Never consumed" = 1,
-    "Consumed sometimes" = 2,
-    "Consumed at least 7 times" = 3
-  ))))
+  mutate(across(c(FGVitACat, FGProteinCat, FGHIronCat), 
+                ~labelled(., labels = c(
+                  "Never consumed" = 1,
+                  "Consumed sometimes" = 2,
+                  "Consumed at least 7 times" = 3
+                ))))
 
-#create table of FGVitACat, FGProteinCat, FGHIronCat by ADM1
+data <- data %>%
+  mutate(across(c(FGVitACat, FGProteinCat, FGHIronCat),
+                ~factor(., levels = c(1, 2, 3),
+                        labels = c("Never consumed", "Consumed sometimes", 
+                                   "Consumed at least 7 times"))))
 
-#make plot
-rcsi_barplot <- rcsi_admin1_table_long %>% 
-ggplot() +
-  geom_col(aes(x = ADMIN1Name, 
-               y = perc,
-               fill = FCSCat21), 
-               width = 0.7) +
-  geom_text(aes(x = ADMIN1Name,
-                y = perc,
-                color = FCSCat21,
-                label = perc),
-                position = position_stack(vjust = 0.5),
-                show.legend = FALSE,
-                size = 10/.pt,) + 
+# Create table of FGVitACat, FGProteinCat, FGHIronCat by ADMIN1 ---------------#
+
+fcsn_table <- data %>%
+  tbl_summary(by = ADMIN1, 
+              include = c(FGVitACat, FGProteinCat, FGHIronCat),
+              type = list(where(is.numeric) ~ "continuous",
+                          where(is.factor)  ~ "categorical"),
+              statistic = list(all_continuous() ~ "{mean}",
+                               all_categorical() ~ "{n}({p}%)"),
+              missing = "no")%>%
+  modify_header(update = list(label ~ "**Variable**"))%>%
+  bold_labels()%>%
+  italicize_labels()%>%
+  italicize_levels() %>%
+  modify_spanning_header(c("stat_0") ~ "**n%**")
+fcsn_table
+
+# Create bar graph of VitACat, FGProteinCat, FGHIronCat -----------------------# 
+
+## adjust data format ---------------------------------------------------------#
+
+data_long <- tidyr::pivot_longer(data, 
+                                 cols = c(FGVitACat, FGProteinCat, FGHIronCat), 
+                                 names_to = "Variable")
+
+## Create the bar graph -------------------------------------------------------# 
+ggplot(data_long, 
+       aes(x = Variable, 
+           fill = factor(value))) +
+  geom_bar(width = 0.5, 
+           position = "stack") +
+  scale_fill_wfp_b(palette = "pal_stoplight_3pt", 
+                   labels = c("Never consumed", "Consumed sometimes", 
+                              "Consumed at least 7 times")) +
+  geom_text(aes(label = scales::percent(..count../tapply(..count.., ..x.., sum)
+                                        [..x..]), 
+                y = ..count.., 
+                group = factor(value)), 
+            stat = "count", 
+            position = position_stack(vjust = 0.5), 
+            color = "white",
+            size = 3.5) +
   scale_color_manual(values = c(main_white, main_black, main_white)) +
   labs(
-    title = "Household Food Consumption Score Classification by State | April 2023",
-    subtitle = "Relative Proportion of Households per FCS Classification by State in Fake Country",
-    caption = "Source: Emergency Food Security Assessment, data collected April 2023"
-  ) + 
-  scale_fill_wfp_b(palette = "pal_stoplight_3pt") + 
+    title = "Household Food Consumption Score for Nutrition",
+    subtitle = "Vitamin A, Protein and Iron-Rich Foods by State",
+    caption = "Source: Emergency Food Security Assessment, data collected May 2023",
+    x = "Categories",
+    y = "Count"
+  ) +
   theme_wfp(grid = "XY",
             axis = FALSE,
             axis_title = FALSE)
-            
