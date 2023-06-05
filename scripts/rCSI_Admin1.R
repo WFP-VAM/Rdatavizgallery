9e489addc64fdd1a75ca47748df1ea0c7f86f4a1
olanderb <billyolander@gmail.com>
2023-06-05 17:50:02 -0600

    Update rCSI_Admin1.R
    added Will's code with some tweaks

--- scripts/rCSI_Admin1.R
+++ scripts/rCSI_Admin1.R
@@ -12,13 +12,15 @@
 library(tidyverse)
 library(labelled)
 library(haven)
+library(scales)
+
 # WFP themes package will be updated frequently - reinstall it everytime for now
 library(devtools)
 install_github("WFP-VAM/wfpthemes")
 library(wfpthemes)
 
 # Load Sample Data ------------------------------------------------------------#
-data <- haven::read_sav("Documents/GitHub/Rdatavizgallery/data/sampledataenglish.sav")
+data <- haven::read_sav("data/sampledataenglish.sav")
 
 # Calculate rCSI --------------------------------------------------------------# 
 #Script copied and pasted from 
@@ -41,27 +43,30 @@ rcsi_admin1_table_long <- data %>%
   summarise(meanrCSI = round(mean(rCSI),1))
 
 # Create bar graph of rCSI ----------------------------------------------------# 
-ggplot(rcsi_admin1_table_long) +
+rcsi_admin_barplot <- rcsi_admin1_table_long %>% ggplot() +
   geom_col(aes(
-    x = reorder(ADMIN1Name_lab, meanrCSI),
-    y = meanrCSI,
+    x = meanrCSI,
+    y = reorder(ADMIN1Name_lab, meanrCSI),
   ),
   fill = wfp_pal(n = 1, "pal_blue"),
-  width = 0.5
+  width = 0.8
   ) +
   labs(
-    title = "Mean rCSI by State | April 2023",
-    subtitle = "Mean reduced Coping Strategy Index (rCSI) by State",
-    y = "rCSI",
-    caption = "Source: Emergency Food Security Assessment, data collected April 2023",
-  ) + 
-  geom_text(aes(x = ADMIN1Name_lab,
-                y = meanrCSI, 
-                label = meanrCSI),
-            vjust = -0.5,
-            size = 10/.pt,
+    title = "Consumption Based Reduced Coping Strategy Index (rCSI) by State | April 2023",
+    subtitle = "Average rCSI  per Household by State",
+    x = "rCSI",
+    y = "State",
+    caption = "Source: Emergency Food Security Assessment, data collected April 2023"
+  ) + geom_text(aes(x = meanrCSI,
+                    y = ADMIN1Name_lab, label = meanrCSI),
+                hjust = -0.5,
+                size = 8 / .pt
   ) +
-  theme_wfp(grid = "Y", 
-                axis_text = "XY", 
-                axis_title = "Y", 
-                axis_ticks = "Y") 
+  scale_x_continuous(
+    expand = expansion(c(0, 0.1)),
+    breaks = pretty_breaks(n = 7),
+    labels = label_number()
+  ) + theme_wfp(grid = FALSE, axis = "y", axis_title = FALSE, axis_text = "y")
+
+
+rcsi_admin_barplot 
